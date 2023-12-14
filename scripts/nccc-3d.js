085b13151c6d9b8e2c96ff0f7448f46cc9ef2e88
gnehs <jayuiop@gmail.com>
2023-12-15 00:46:03 +0800

    updated module

--- scripts/nccc-3d.js
+++ scripts/nccc-3d.js
@@ -1,23 +1,5 @@
-async function editResponse() {
-  let body = $response.body;
-  // change type to number
-  body = body.replace(/type="password"/g, 'type="number"');
-  $done({ body });
-  $notification.post(
-    "🎉 信用卡 3D 驗證自動填寫修正",
-    "已修正信用卡 3D 驗證自動填寫問題"
-  );
-}
-
-(async () => {
-  console.log("ℹ️ 信用卡 3D 驗證自動填寫修正 v20231215.1");
-  try {
-    if (isManualRun(true, false)) {
-      throw "請勿手動執行此腳本";
-    }
-    await editResponse();
-  } catch (error) {
-    handleError(error);
-  }
-  $done({});
-})();
+let body = $response.body;
+// change type to number
+body = body.replace(/type="password"/g, 'type="number"');
+$notification.post("Response edited");
+$done({ body });
