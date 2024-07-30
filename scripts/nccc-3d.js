3392390fd368a573279a1e2b9eebb6e01554acd4
Yen-Ying Lee <yenying.lee.1033@gmail.com>
2024-07-30 17:24:07 +0800

    fix: replace while body is not undefine

--- scripts/nccc-3d.js
+++ scripts/nccc-3d.js
@@ -1,5 +1,7 @@
 let body = $response.body;
-// change type to number
-body = body.replace(/type="password"/g, 'type="number"');
-body = body.replace(/autocomplete="off"/g, 'autocomplete="one-time-code"');
+if (body) {
+  // change type to number
+  body = body.replace(/type="password"/g, 'type="number"');
+  body = body.replace(/autocomplete="off"/g, 'autocomplete="one-time-code"');
+}
 $done({ body });
