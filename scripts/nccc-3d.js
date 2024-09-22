bd65a46e77532101e9d5f66c3562f948a9afaf36
gnehs <gnehs@gnehs.net>
2024-09-22 15:12:17 +0800

    changed type to text

--- scripts/nccc-3d.js
+++ scripts/nccc-3d.js
@@ -1,7 +1,7 @@
 let body = $response.body;
 if (body) {
   // change type to number
-  body = body.replace(/type="password"/g, 'type="number"');
+  body = body.replace(/type="password"/g, 'type="text"');
   body = body.replace(/autocomplete="off"/g, 'autocomplete="one-time-code"');
 }
 $done({ body });
